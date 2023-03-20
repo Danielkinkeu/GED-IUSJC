4fea72d7d0dd89d7fae51e3a00eac5bcad84c7dc
Danielkinkeu <kinkeufranck@gmail.com>
2023-03-20 03:27:01 +0100

    models_correction

--- GED/API_GES/models.py
+++ GED/API_GES/models.py
@@ -2,34 +2,34 @@ from django.db import models
 
 # Create your models here.
 class Categories(models.Model):
-    code_categorie = models.CharField(max_length=50,default ='')
+    code_categorie = models.CharField(max_length=50, default ='')
     nom_categorie  = models.CharField(max_length = 30)
     type_doc= models.CharField(max_length = 30)
     
     
     
 class Historique(models.Model):
-    id_document = models.CharField(max_length=50)
-    nom_doc  = models.CharField(max_length = 30)
+    # id_document = models.CharField()
+    nom_doc_h  = models.CharField(max_length=50, default=" ")
     
     
   
     
-class Document(models.Model):
-    id_doc = models.CharField(max_length=50)
-    nom_doc  = models.CharField(max_length = 30)
+class Documents(models.Model):
+    # id_doc = models.CharField(max_length=50, default=" ")
+    nom_doc  = models.CharField(max_length=50, default=" ")
     date_creation = models.DateField()
     date_archivage = models.DateField()
-    traitement = models.CharField()
-    type_doc = models.CharField()
-    nom_modifieur = models.CharField(max_lenght=50)
+    traitement = models.CharField(max_length=50, default=" ")
+    type_doc = models.CharField(max_length=50, default=" ")
+    nom_modifieur = models.CharField(max_length=50, default=" ")
     date_creation_doc = models.DateField()
-    description_doc = models.CharField()
+    description_doc = models.CharField(max_length=50, default=" ")
     
    
     
 class Visibilite(models.Model):
-    id_doc = models.CharField(max_length=50)
-    code_utilisateur  = models.CharField(max_length = 30, default = '')
+    # id_doc = models.CharField(max_length=50, default=" ")
+    code_utilisateur  = models.CharField(max_length = 30,  default = '')
     
    
